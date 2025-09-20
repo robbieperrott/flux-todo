0c66064f82f86d6d76698463ef6518824c0a2852
Robbie Perrott <robbieperrott@gmail.com>
2025-09-20 21:23:41 +0200

    Update task name

--- app/api/tasks/[id]/route.ts
+++ app/api/tasks/[id]/route.ts
@@ -5,11 +5,11 @@ export async function PUT(
     request: NextRequest, context: { params: Promise<{ id: string; }>; })
 {
     const { id } = await context.params;
-    const { complete } = await request.json();
+    const { complete, description } = await request.json();
 
     const updatedTask = await prisma.task.update({
         where: { id: parseInt(id) },
-        data: { complete },
+        data: { complete, description },
     });
 
     return new Response(JSON.stringify(updatedTask), { status: 201 });
--- app/components/TaskCard.tsx
+++ app/components/TaskCard.tsx
@@ -1,8 +1,12 @@
 "use client"
 
+import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import { Checkbox } from "@/components/ui/checkbox";
-import { X } from "lucide-react";
+import { Dialog, DialogClose, DialogContent, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { PenLine, X } from "lucide-react";
+import { useState } from "react";
 
 interface TaskCardProps {
   task: {complete: boolean;
@@ -25,7 +29,7 @@ export default function TaskCard(props: TaskCardProps) {
         location.reload();
     }
 
-    async function deleteList(e: React.FormEvent) {
+    async function deleteTask(e: React.FormEvent) {
         e.preventDefault();
 
         await fetch(`/api/tasks/${task.id}`, {
@@ -36,13 +40,62 @@ export default function TaskCard(props: TaskCardProps) {
     }
 
     return <Card className="p-3">
-        <CardContent className="flex justify-between items-center">
-            <div className="flex px-2 items-center gap-4">
+        <CardContent className="flex px-2 justify-between items-center">
+            <div className="flex items-center gap-4">
                 <Checkbox onClick={updateTask} checked={task.complete}/>
                 {task.description}
             </div>
-            <X size={20} onClick={deleteList}/>
-            
+            <div className="flex items-center gap-4">
+                <EditTaskDescription taskId={task.id}/>
+                <X size={20} onClick={deleteTask}/>
+            </div>
         </CardContent>
     </Card>
+}
+
+interface EditTaskDescriptionProps {
+    taskId: number;
+}
+
+function EditTaskDescription(props: EditTaskDescriptionProps) {
+    const {taskId} = props;
+  const [description, setDescription] = useState("");
+
+  async function updateTaskName(e: React.FormEvent) {
+        e.preventDefault();
+        if (!description) return;
+
+        await fetch(`/api/tasks/${taskId}`, {
+            method: "PUT",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ description }),
+        });
+
+        setDescription("");
+        location.reload();
+    }
+
+  return <Dialog>
+            <DialogTrigger asChild>
+            <PenLine size={20}/>
+            </DialogTrigger>
+            <DialogContent className="sm:max-w-[425px]">
+                    <DialogHeader>
+                        <DialogTitle>Change Task Name</DialogTitle>
+                    </DialogHeader>
+                    <Input
+                        className="my-4"
+                        type="text"
+                        placeholder="Enter a new task name..."
+                        value={description}
+                        onChange={(e) => setDescription(e.target.value)}
+                    />
+                    <DialogFooter>
+                        <DialogClose asChild>
+                            <Button variant="outline">Cancel</Button>
+                        </DialogClose>
+                        <Button onClick={updateTaskName}>Save</Button>
+                    </DialogFooter>
+            </DialogContent>
+        </Dialog>
 }

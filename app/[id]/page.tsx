4107c769dd531af96c78ecfcc4dc4a6b6dc9a2a1
Robbie Perrott <robbieperrott@gmail.com>
2025-09-20 16:18:28 +0200

    Style list page

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -1,4 +1,6 @@
 import prisma from "@/lib/prisma";
+import { Task } from "../generated/prisma/client";
+import { Card, CardContent } from "@/components/ui/card";
 
 
 export default async function ListPage({
@@ -11,12 +13,23 @@ export default async function ListPage({
     const list = await prisma.list.findUnique({where: {id: parseInt(id)}, include: { tasks: true }})
 
     return <div>
-        {list?.title}
-        <ul>
-            {list?.tasks.map((task) => <li key={task.id}>
-                {task.description}
-            </li>)}
-        </ul>
-        
+        <div className="text-xl mb-4">
+          {list?.title}
+          </div>
+          <div className="flex flex-col gap-4">
+            {list?.tasks.map((task) => <TaskCard key={task.id} description={task.description} />)}
+          </div>
     </div>
+}
+
+interface TaskCardProps {
+  description: string;
+}
+
+function TaskCard(props: TaskCardProps) {
+  const {description} = props;
+
+  return <Card className="p-3">
+    <CardContent>{description}</CardContent>
+  </Card>
 }

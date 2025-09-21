bd3ac0e75c4b92962731406474411ba21cc2d6f7
Robbie Perrott <robbieperrott@gmail.com>
2025-09-21 12:10:28 +0200

    Optimistically update complete checkbox

--- app/components/TaskCard.tsx
+++ app/components/TaskCard.tsx
@@ -5,6 +5,7 @@ import { Checkbox } from "@/components/ui/checkbox";
 import { X } from "lucide-react";
 import EditTaskDescription from "./EditTaskDescription";
 import { deleteTask, updateTask } from "../actions";
+import { useOptimistic, useTransition } from "react";
 
 interface TaskCardProps {
   task: {complete: boolean;
@@ -15,10 +16,23 @@ interface TaskCardProps {
 export default function TaskCard(props: TaskCardProps) {
     const {task} = props;
 
-    async function updateTaskComplete(e: React.FormEvent) {
+    const [isPending, startTransition] = useTransition();
+
+    const [optimisticComplete, updateOptimisticComplete] = useOptimistic<
+        boolean,
+        boolean
+    >(
+        task.complete,
+        (_, optimisticCompleteValue) => {
+            return optimisticCompleteValue;
+        }
+    );
+
+    const updateTaskComplete = (e: React.FormEvent) => startTransition(async () => {
         e.preventDefault();
+        updateOptimisticComplete(!task.complete)
         await updateTask({id: task.id, complete: !task.complete}, location.pathname);
-    }
+    })
 
     async function handleDelete(e: React.FormEvent) {
         e.preventDefault();
@@ -28,7 +42,11 @@ export default function TaskCard(props: TaskCardProps) {
     return <Card className="p-3">
         <CardContent className="flex px-2 justify-between items-center">
             <div className="flex items-center gap-4">
-                <Checkbox onClick={updateTaskComplete} checked={task.complete}/>
+                <Checkbox
+                    disabled={isPending}    // Don't allow further changes while transition is happening
+                    onClick={updateTaskComplete}
+                    checked={optimisticComplete}
+                />
                 {task.description}
             </div>
             <div className="flex items-center gap-4">

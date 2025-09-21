8113b4199aade5e966ff7f68465977bdebaf77b8
Robbie Perrott <robbieperrott@gmail.com>
2025-09-21 08:18:47 +0200

    Simplify server actions

--- app/actions.ts
+++ app/actions.ts
@@ -4,35 +4,33 @@ import prisma from '@/lib/prisma';
 import { auth } from '@clerk/nextjs/server';
 import { revalidatePath } from 'next/cache';
 
+
 export async function createList(title: string, pathname: string) {
-const { userId: clerkId } = await auth();
-  if (!clerkId) return new Response("Unauthorized", { status: 401 });
+    const { userId: clerkId } = await auth();
+    if (!clerkId) throw new Error("Unauthorized");
 
-  const user = await prisma.user.findUnique({
-    where: { clerkId },
-  });
+    const user = await prisma.user.findUnique({
+        where: { clerkId },
+    });
 
-  if (!user) return new Response("User not found", { status: 404 });
+    if (!user) throw new Error("User not found");
 
-  await prisma.list.create({
-    data: {
-      title,
-      userId: user.id,
-    },
-  });
-   revalidatePath(pathname);
+    await prisma.list.create({
+        data: {
+        title,
+        userId: user.id,
+        },
+    });
+    revalidatePath(pathname);
 }
 
 export async function createTask(description: string, listId: number, pathname: string) {
-    const { userId: clerkId } = await auth();
-  if (!clerkId) return new Response("Unauthorized", { status: 401 });
+    await prisma.task.create({
+        data: {
+        description,
+        listId,
+        },
+    });
 
-  await prisma.task.create({
-    data: {
-      description,
-      listId,
-    },
-  });
-
-  revalidatePath(pathname);
+    revalidatePath(pathname);
 }

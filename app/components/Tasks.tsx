eddaa3b4eeb3bbcf9fb64a96d1f497ce7f942334
Robbie Perrott <robbieperrott@gmail.com>
2025-09-21 16:27:59 +0200

    Add task optimistically

--- app/components/Tasks.tsx
+++ app/components/Tasks.tsx
@@ -1,10 +1,12 @@
 "use client"
+import { useOptimistic, useTransition } from "react";
 import { createTask } from "../actions";
 import { ListWithTasks } from "../types";
 import DeleteListButton from "./DeleteListButton";
 import NewTaskButton from "./NewTaskButton";
 import TaskCard from "./TaskCard";
 import UpdateListTitleButton from "./UpdateListTitleButton";
+import { Task } from "../generated/prisma/browser";
 
 interface TasksProps {
     list: ListWithTasks;
@@ -13,11 +15,28 @@ interface TasksProps {
 export default function Tasks(props: TasksProps) {
     const {list} = props;
     
-    const sortedTasks = list.tasks.sort((a, b) => a.createdAt.getTime() - b.createdAt.getTime());
+    const [isPending, startTransition] = useTransition();
+    
+    const [optimisticTasks, addOptimisticTask] = useOptimistic<
+        Task[],
+        string
+    >(
+        list.tasks,
+        (currentTasks, newTaskDescription) => [...currentTasks, {
+            id: Date.now(), // Temporary optimistic ID,
+            description: newTaskDescription,
+            createdAt: new Date(),
+            listId: list.id,
+            complete: false,
+        }]
+    )
 
-    const handleSubmitNewTask = async (description: string) => {
+    const handleSubmitNewTask = async (description: string) => startTransition(async () => {
+        addOptimisticTask(description);
         await createTask(description, list.id, location.pathname);
-    }
+    });
+
+    const sortedTasks = optimisticTasks.sort((a, b) => a.createdAt.getTime() - b.createdAt.getTime());
 
     return <><div className="flex justify-between mb-4">
         <div className="text-xl font-semibold my-auto">
@@ -26,7 +45,7 @@ export default function Tasks(props: TasksProps) {
         <div className="flex gap-4 items-center">
           <UpdateListTitleButton listId={list.id}/>
           <DeleteListButton listId={list.id}/>
-          <NewTaskButton isPending={false} onSubmit={handleSubmitNewTask}/>
+          <NewTaskButton isPending={isPending} onSubmit={handleSubmitNewTask}/>
         </div>
       </div><div className="flex flex-col gap-4">
         {sortedTasks.map((task) => <TaskCard key={task.id} task={task}/>)}

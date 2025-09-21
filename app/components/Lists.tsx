cf2ba87fcfc641bd32bcaafa02026ac2ac381418
Robbie Perrott <robbieperrott@gmail.com>
2025-09-21 11:58:53 +0200

    Move lists display to its own component, and optimistically add new lists to the UI

---
+++ app/components/Lists.tsx
@@ -0,0 +1,92 @@
+"use client"
+
+import { useOptimistic, useState, useTransition } from "react";
+import { List, Task } from "../generated/prisma/browser";
+import Link from "next/link";
+import { Card, CardContent } from "@/components/ui/card";
+import { createList } from "../actions";
+import { Dialog, DialogClose, DialogContent, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Plus } from "lucide-react";
+
+type ListWithTasks = List & {tasks: Task[]}
+
+interface ListsProps {
+    lists: ListWithTasks[];
+    userId: number;
+}
+
+export default function Lists(props: ListsProps) {
+    const {lists, userId} = props;
+
+    const [newListTitle, setNewListTitle] = useState("");
+    const [dialogOpen, setDialogOpen] = useState(false);
+    const [isPending, startTransition] = useTransition();
+
+    const [optimisticLists, addOptimisticList] = useOptimistic<
+        ListWithTasks[],
+        string
+    >(
+        lists,
+        (state, newListTitle) => [...state, {
+            id: Date.now(), // Temporary optimistic ID
+            userId,
+            title: newListTitle,
+            createdAt: new Date(),
+            tasks: [],
+        }]
+    )
+
+     const handleSubmit = (e: React.FormEvent) => startTransition(async () => {
+        setNewListTitle("");
+        setDialogOpen(false);
+        e.preventDefault();
+        addOptimisticList(newListTitle);
+        await createList(newListTitle, location.pathname);
+    });
+
+    const fractionCompleteString = (tasks: Task[]) => tasks.length ? `${tasks.filter(task => task.complete).length}/${tasks.length}` : '-'
+
+    return <>
+        <div className="flex flex-col gap-4">
+            {optimisticLists.map((list) => (
+            <Link href={`/${list.id}`} key={list.id}>
+                <Card className="p-3">
+                <CardContent className="flex justify-between">
+                    {list.title}
+                    <div>{fractionCompleteString(list.tasks)}</div>
+                    </CardContent>
+                </Card>
+            </Link>
+            ))}
+        </div>
+        <div className="mt-8">
+            <Dialog open={dialogOpen} onOpenChange={setDialogOpen}>
+            <DialogTrigger asChild>
+                <Button><Plus/>New List</Button>
+            </DialogTrigger>
+            <DialogContent showCloseButton={false}>
+                <form onSubmit={handleSubmit}>
+                    <DialogHeader>
+                        <DialogTitle>New List</DialogTitle>
+                    </DialogHeader>
+                    <Input
+                            className="my-4"
+                            type="text"
+                            placeholder="Enter a new list name..."
+                            value={newListTitle}
+                            onChange={(e) => setNewListTitle(e.target.value)}
+                    />
+                    <DialogFooter>
+                        <DialogClose asChild>
+                            <Button variant="outline">Cancel</Button>
+                        </DialogClose>
+                        <Button type="submit" disabled={isPending || newListTitle === ""}>Create</Button>
+                    </DialogFooter>
+                </form>
+            </DialogContent>
+        </Dialog>
+        </div>
+        </>
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,6 @@
 import { currentUser } from "@clerk/nextjs/server";
 import prisma from "@/lib/prisma";
-import Link from "next/link";
-import NewListButton from "./components/NewListButton";
-import { Card, CardContent } from "@/components/ui/card";
-import { Task } from "./generated/prisma/client";
+import Lists from "./components/Lists";
 
 export default async function ListsPage() {
   const user = await currentUser();
@@ -15,25 +12,9 @@ export default async function ListsPage() {
     orderBy: [{createdAt: "asc"}]
   });
 
-  const fractionCompleteString = (tasks: Task[]) => tasks.length ? `${tasks.filter(task => task.complete).length}/${tasks.length}` : '-'
-
   return (
     <main>
-      <div className="flex flex-col gap-4">
-        {lists.map((list) => (
-          <Link href={`/${list.id}`} key={list.id}>
-            <Card className="p-3">
-              <CardContent className="flex justify-between">
-                  {list.title}
-                  <div>{fractionCompleteString(list.tasks)}</div>
-                </CardContent>
-            </Card>
-          </Link>
-        ))}
-      </div>
-      <div className="mt-8">
-        <NewListButton/>
-      </div>
+      <Lists lists={lists} userId={parseInt(user.id)}/>
     </main>
   );
 }
